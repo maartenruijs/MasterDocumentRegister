cdcc60cc1ae84a9f790f95a926b784b0aa759560
Maarten Ruijs <maartenruijs@gmail.com>
2018-02-06 21:41:51 +0100

    Added main pages for revision addition

---
+++ MasterDocumentRegister/add_rev_success.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <link rel="stylesheet" type="text/css" href="./scripts/style.css" />
+    <script type="text/javascript" src="./scripts/script.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <title>New Document Succesfully Created - Master Document Register - ARC-SES & Offshore Independents</title>
+</head>
+
+<?php include './mains/main_header.php';?>
+<?php include './mains/main_navigation.php';?>
+<?php include "./other/connection.php" ; ?>
+
+<body>
+    <div id="wrapper">
+        <div id="div_aside">
+            <?php include("./mains/main_aside.php"); ?>
+        </div>
+        <div id="main">
+            <?php include("./divs/div_add_rev_success.php"); ?>
+        </div>
+    </div>
+</body>
+
+<?php include './mains/main_footer.php';?>
+</html>
